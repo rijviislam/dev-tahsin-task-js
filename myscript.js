9e28455cd9c5b3766100175b60bd26708c544f5b
Rijvi Islam <rijviislam54@gmail.com>
2023-02-15 03:27:09 +0600

    feat: done

--- myscript.js
+++ myscript.js
@@ -4,16 +4,26 @@ function main() {
     const modalContainer = document.getElementById('modalContainer');
     const modalContainer2 = document.getElementById('modalContainer2');
     const backend = document.getElementById('backend');
+    const gitnadgithub = document.getElementById('gitnadgithub');
+    const modalContainer3 = document.getElementById('modalContainer3');
     // const close = document.getElementById('close');
 
     frontend.addEventListener('click', function () {
         modalContainer.classList.add("show");
+        modalContainer2.classList.remove('show1');
+        modalContainer3.classList.remove('show3');
+
     });
     backend.addEventListener('click', function () {
         modalContainer2.classList.add('show1');
-        // modalContainer.classList.remove('show');
-    })
+        modalContainer.classList.remove('show');
+        modalContainer3.classList.remove('show3');
+    });
+    gitnadgithub.addEventListener('click', function () {
+        modalContainer.classList.remove('show');
+        modalContainer2.classList.remove('show1');
+        modalContainer3.classList.add('show3');
+    });
 
-    console.log(backend);
 }
 main();
